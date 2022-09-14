8b2676ee795c7d4fcc2f7437ff13ca195e1084e8
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2022-09-14 22:47:02 +0200

    working search view

--- app/src/main/java/moviedleapp/main/fragments/MoviedleFragment.kt
+++ app/src/main/java/moviedleapp/main/fragments/MoviedleFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import moviedleapp.main.Movie
@@ -16,7 +18,8 @@ import moviedleapp.main.listView.RecyclerViewAdapter
 
 class MoviedleFragment : Fragment() {
 
-    private lateinit var recyclerView :RecyclerView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,14 +33,13 @@ class MoviedleFragment : Fragment() {
 
         val moviedleListener = object : MoviedleListener {
             override fun addMoviesToListView(moviesList: ArrayList<Movie>) {
-            createMoviesListView(moviesList)
+                createMovieListView(moviesList)
             }
         }
         val fragmentController = MoviedleFragmentController(moviedleListener)
     }
 
-    private fun createMoviesListView(moviesList: ArrayList<Movie>) {
-        iterateMoviesTEMP(moviesList) // TEMP
+    private fun createMovieListView(moviesList: ArrayList<Movie>) {
         val listModelArray: ArrayList<ListModel> = ArrayList()
         val imageTEMP =
             R.drawable.place_holder_nodpi //TEMP, in future change it to image assigned to the movie
@@ -48,13 +50,40 @@ class MoviedleFragment : Fragment() {
         for (movie in moviesList) {
             listModelArray.add(ListModel(movie.getTitle(), imageTEMP))
         }
-        recyclerView.adapter = RecyclerViewAdapter(listModelArray)
+
+        searchView = requireView().findViewById(R.id.search_view)
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    recyclerView.adapter =
+                        RecyclerViewAdapter(filterMovies(newText, listModelArray))
+                } else {
+                    Toast.makeText(activity, "No movie found", Toast.LENGTH_SHORT).show()
+                }
+                return true
+            }
+
+        })
     }
 
-    private fun iterateMoviesTEMP(movies : ArrayList<Movie>){
-        for(movie in movies){
-            println(movie.getTitle())
+    private fun filterMovies(
+        input: String,
+        listModelArray: ArrayList<ListModel>
+    ): ArrayList<ListModel> {
+        val filteredList: ArrayList<ListModel> = ArrayList()
+        if (input.isNotBlank()) {
+            for (listModel in listModelArray) {
+                if (listModel.getTitle().startsWith(input.lowercase())) {
+                    filteredList.add(listModel)
+                }
+            }
         }
-        println("Movies list size ${movies.size}")
+        return filteredList
     }
+
 }
